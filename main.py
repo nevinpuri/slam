ec991d6533e24da2745583d0f6b4ca4799bd9cab
Nevin1901 <nevinpuri1901@gmail.com>
2022-04-13 23:43:22 +0200

    got pangolin working

--- main.py
+++ main.py
@@ -8,15 +8,21 @@ pn.CreateWindowAndBind('Main', 640, 480)
 gl.glEnable(gl.GL_DEPTH_TEST)
 
 scam = pn.OpenGlRenderState(
-        pn.ProjectionMatrix(640, 480, 420, 420, 320, 240, 0.2, 100),
-        pn.ModelViewLookAt(-2, 2, -2, 0, 0, 0, pn.AxisDirection.AxisY))
+        pn.ProjectionMatrix(640, 480, 420, 420, 320, 240, 0.1, 1000),
+        pn.ModelViewLookAt(-0, 0.5, -3, 0, 0, 0, pn.AxisDirection.AxisY))
 
 handler = pn.Handler3D(scam)
-
-dcam = pn.CreateDisplay()
-print(dir(dcam))
-dcam.SetBounds(0.0, 1.0, 0.0, 1.0, -640.0/480.0)
-dcam.SetBounds(handler)
+dcam = (
+        pn.CreateDisplay()
+        .SetBounds(
+            pn.Attach(0),
+            pn.Attach(1),
+            pn.Attach.Pix(180),
+            pn.Attach(1),
+            -640/480,
+            )
+        .SetHandler(handler)
+        )
 
 if vid.isOpened() == False:
     print("Error opening 0")
@@ -40,14 +46,13 @@ while True:
     gl.glClearColor(1.0, 1.0, 1.0, 1.0)
     dcam.Activate(scam)
 
-    pangolin.glDrawColouredCube()
+    pn.glDrawColouredCube()
 
     points = np.random.random((100000, 3)) * 10
     gl.glPointSize(2)
     gl.glColor3f(1.0, 0.0, 0.0)
-    pangolin.DrawPoints(points)
 
-    pangolin.FinishFrame()
+    pn.FinishFrame()
 
     cv.imshow("frame", img2)
     key = cv.waitKey()
