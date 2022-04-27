8ee48a59c56ac0ff1022bdfab10523cde02997b9
Nevin1901 <nevinpuri1901@gmail.com>
2022-04-27 15:28:48 +0200

    remove display

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from display import Display
 from multiprocessing import freeze_support
 
 if __name__ == "__main__":
-    display = Display()
+    # display = Display()
     freeze_support()
     orb = cv.ORB_create(nfeatures=2000, scaleFactor=1.2)
 
@@ -80,6 +80,8 @@ if __name__ == "__main__":
         f, mask = cv.findFundamentalMat(old_frame_planar, new_frame_planar, cv.FM_LMEDS)
 
         h = np.array(h[0])
+        print(h)
+        print(f)
         f = np.array(f)
 
         rh = np.sum(h) / (np.sum(h) + np.sum(f))
@@ -90,7 +92,7 @@ if __name__ == "__main__":
         else:
             chosen = f
 
-        print(chosen)
+        # print(chosen)
 
         # img3 = cv.drawKeypoints(cur_frame, kp_current, None, color=(0, 255, 0))
         img3 = cv.drawMatchesKnn(prev_frame, kp_prev, gray, kp_current, matches[:10], None, (0, 255, 0))
