776fc26cd022e99cacbb9b87908ab9f41bf93c2b
Nevin1901 <nevinpuri1901@gmail.com>
2022-09-12 18:09:53 +0200

    matches

--- slam.py
+++ slam.py
@@ -5,6 +5,17 @@ if __name__ == "__main__":
     cap = cv.VideoCapture("0.hevc")
     orb = cv.ORB_create()
 
+    FLANN_INDEX_LSH = 6
+    index_params = dict(algorithm = FLANN_INDEX_LSH,
+            table_number = 6,
+            key_size = 12,
+            multi_probe_level = 1)
+
+    search_params = dict(checks=50)
+
+    flann = cv.FlannBasedMatcher(index_params, search_params)
+
+
     prev_frame = None
     while True:
         ret, frame = cap.read()
@@ -19,11 +30,29 @@ if __name__ == "__main__":
         kp1, des1 = orb.detectAndCompute(prev_frame, None)
         kp2, des2 = orb.detectAndCompute(frame, None)
 
-        print(kp1)
+        matches = flann.knnMatch(des1, des2, k=2)
 
-        frame = cv.drawKeypoints(frame, kp2, None, color=(0, 255, 0), flags=0)
+        # print(matches)
 
-        cv.imshow("frame", frame)
+        matchesMask = [[0,0] for i in range(len(matches))]
+
+        for i, k in enumerate(matches):
+            if len(k) < 2:
+                continue
+
+            if k[0].distance < 0.7 * k[1].distance:
+                matchesMask[i] = [1,0]
+
+
+        draw_params = dict(matchColor = (0, 255, 0),
+                singlePointColor = (255, 0, 0),
+                matchesMask = matchesMask,
+                flags = cv.DrawMatchesFlags_DEFAULT)
+
+        # frame = cv.drawKeypoints(frame, kp2, None, color=(0, 255, 0), flags=0)
+        matches_frame = cv.drawMatchesKnn(prev_frame, kp1, frame, kp2, matches, None, **draw_params)
+
+        cv.imshow("frame", matches_frame)
         prev_frame = frame
 
         key = cv.waitKey()
