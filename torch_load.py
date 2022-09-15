a9c40dcb190d149da1e019b5cf725e31fd2b1d5d
Nevin1901 <nevinpuri1901@gmail.com>
2022-09-15 18:39:22 +0200

    model test

--- torch_load.py
+++ torch_load.py
@@ -2,17 +2,28 @@ import numpy as np
 import cv2 as cv
 import torch
 from fastseg import MobileV3Small
-from fastseg.image import colorize
+from fastseg.image import colorize, blend
+from PIL import Image
 
-cap = cv.VideoCapture("0.hevc")
-model = MobileV3Small(num_classes=3, use_aspp=True)
-state_dict = torch.load("/home/nevin/Desktop/model.pth")
-model.from_pretrained("/home/nevin/Desktop/model.pth")
+cap = cv.VideoCapture("2.hevc")
+model = MobileV3Small.from_pretrained()
+# state_dict = torch.load("/home/nevin/Desktop/model.pth")
+# model.from_pretrained("/home/nevin/Desktop/model.pth")
 # model.load_state_dict(state_dict)
 model.cuda()
 model.eval()
 
-# model = MobileV3Small.from_pretrained("/home/nevin/Desktop/model.pt")
+# # model = MobileV3Small.from_pretrained("/home/nevin/Desktop/model.pt")
+# img = cv.imread(
+#     "/home/nevin/Downloads/68747470733a2f2f692e696d6775722e636f6d2f4d4a4137564d4e2e706e67.png"
+# )
+
+# pred = model.predict_one(np.asarray(img))
+# pred = colorize(pred)
+
+# cv.imshow("frame2", np.asarray(pred))
+
+# cv.waitKey()
 
 
 while True:
@@ -21,10 +32,12 @@ while True:
     if ret != True:
         break
 
+    print(frame.shape)
+
     pred = model.predict_one(np.asarray(frame))
+
     # pred = model.predict_one(np.asarray(frame))
-    count = np.count_nonzero(pred == 1)
-    print(count)
+    # count = np.count_nonzero(pred == 1)
     colorized = colorize(pred)
     cv.imshow("frame1", frame)
     cv.imshow("frame", np.asarray(colorized))
